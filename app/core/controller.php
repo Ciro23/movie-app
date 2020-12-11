804b6eec3fbbd960b21f9aee72020ba10039cbef
Ciro23 <ciro.sorre23@gmail.com>
2020-12-11 13:08:10 +0100

    model name case sensitive, added comments

--- app/core/controller.php
+++ app/core/controller.php
@@ -2,11 +2,25 @@
 
 class Controller {
 
+    /*
+    * return a new model object
+    *
+    * @param string $model, the model name
+    *
+    * @return object $model
+    */
     protected function model($model) {
-        $model .= "model";
+        $model .= "Model";
+        $model = ucfirst($model);
         return new $model();
     }
 
+    /*
+    * requires the wanted view
+    *
+    * @param string $view, the view name
+    * @param array $data, the data to pass to the view
+    */
     protected function view($view, $data = []) {
         if ($data === null) {
             // renders the pagenotfound view
