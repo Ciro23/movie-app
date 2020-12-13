c11d74021b09b658b7d95b1eacbcce0086c3326c
Ciro23 <ciro.sorre23@gmail.com>
2020-12-13 16:15:25 +0100

    made 404 page more functional

--- app/views/pagenotfound.view.php
+++ app/views/pagenotfound.view.php
@@ -3,9 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Page not found - MVC</title>
+
+        <!-- styles and other files -->
+        <?php include __DIR__ . "/../included/common.included.php" ?>
+        <link rel="stylesheet" href="/assets/styles/css/page-not-found.style.css">
+
+        <title><?= $lang['page_not_found'] ?> - MVC</title>
     </head>
     <body>
-        <p>page not found</p>
+        <?php include __DIR__ . "/../included/nav-bar.included.php" ?>
+        <p><?= $lang['page_not_found'] ?></p>
     </body>
 </html>
