cc38ac559d3469b4d0b7b254ec756cc12654b24b
ciro23 <ciro.sorre23@gmail.com>
2022-01-11 00:27:35 +0100

    added viewcells and renamed variables passed to the view

--- app/views/home.php
+++ app/views/home.php
@@ -1,90 +1,73 @@
-<!DOCTYPE html>
-<html lang="<?= $_COOKIE['language'] ?>" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home - Movie App</title>
+<?= view_cell("\App\Libraries\ViewCells::header") ?>
 
-        <!-- styles and other files -->
-        <?php include_once __DIR__ . "/../included/common.included.php" ?>
-        <link rel="stylesheet" href="/assets/styles/css/home.style.css">
+<div class="movies-container">
+    <?php
+    if (isset($movie_filter)) {
+        echo "<span class='filter-box'>";
+        echo "<a href='/popular'>" . $lang['movie_filter']['popular'] . "</a>";
+        echo "<a href='/now_playing'>" . $lang['movie_filter']['now_playing'] . "</a>";
+        echo "<a href='/top_rated'>" . $lang['movie_filter']['top_rated'] . "</a>";
+        echo "<a href='/upcoming'>" . $lang['movie_filter']['upcoming'] . "</a>";
+        echo "</span>";
+    } else {
+        if ($total_results == 1) {
+            echo "<h2 class='total-results'>" . $lang['total_results']['there_is'] . $total_results . $lang['total_results']["result_for"] . "\"" . $query . "\"</h2>";
+        } else {
+            echo "<h2 class='total-results'>" . $lang['total_results']['there_are'] . $total_results . $lang['total_results']["results_for"] . "\"" . $query . "\"</h2>";
+        }
+    }
+    ?>
 
-        <!-- movie grid animation script -->
-        <script src="/assets/js-scripts/movie-grid.script.js"></script>
-    </head>
+    <div class='movie-grid'>
+        <?php
+        foreach ($results as $index => $movie) {
+            echo "<a href='/movie/" . $movie['id'] . "' class='movie'>";
+            echo "<div>";
 
-    <body>
-        <?php include_once __DIR__ . "/../included/nav-bar.included.php" ?>
+            //$imgPath = App\Models\MovieModel::doesMovieImageExists($movie['poster_path'], "w200");
+            $imgPath = "https://image.tmdb.org/t/p/w200" . $movie['poster_path'];
+            echo "<img src='$imgPath'>";
+            echo "<p class='title'>" . $movie['title'] . "</p>";
+            echo "<span class='vote'>" . $movie['vote_average'] . "</span>";
+            echo "</div>";
+            echo "</a>";
+        }
+        ?>
+    </div>
 
-        <div class="movies-container">
+    <div class="page-filter">
+        <div class="filter-box">
             <?php
-            if (isset($data['movie_filter'])) {
-                echo "<span class='filter-box'>";
-                echo "<a href='/popular'>" . $lang['movie_filter']['popular'] . "</a>";
-                echo "<a href='/now_playing'>" . $lang['movie_filter']['now_playing'] . "</a>";
-                echo "<a href='/top_rated'>" . $lang['movie_filter']['top_rated'] . "</a>";
-                echo "<a href='/upcoming'>" . $lang['movie_filter']['upcoming'] . "</a>";
-                echo "</span>";
+            // determines the base root of the url
+            if (isset($movie_filter)) {
+                $base = $movie_filter . "/";
             } else {
-                if ($data['total_results'] == 1) {
-                    echo "<h2 class='total-results'>" . $lang['total_results']['there_is'] . $data['total_results'] . $lang['total_results']["result_for"] . "\"" . $data['query'] . "\"</h2>";
+                $base = "search/" . $query . "/";
+            }
+
+            // shows previous page
+            if ($page > 1) {
+                $prev = $page - 1;
+                echo "<a href='/" . $base . $prev . "'> < </a>";
+            }
+
+            // shows page numbers
+            for ($i = $minPage; $i <= $maxPage; $i++) {
+                if ($i == $page) {
+                    echo "<a class='current-page'> " . $i . "</a>";
                 } else {
-                    echo "<h2 class='total-results'>" . $lang['total_results']['there_are'] . $data['total_results'] . $lang['total_results']["results_for"] . "\"" . $data['query'] . "\"</h2>";
+                    echo "<a href='/" . $base . $i . "'> " . $i . "</a>";
                 }
             }
+
+            // shows next page
+            if ($page < $total_pages) {
+                $next = $page + 1;
+                echo "<a href='/" . $base . $next . "'> > </a>";
+            }
             ?>
-
-            <div class='movie-grid'>
-                <?php
-                foreach ($data['results'] as $index => $movie) {
-                    echo "<a href='/movie/" . $movie['id'] . "' class='movie'>";
-                    echo "<div>";
-
-                    $imgPath = MovieModel::doesMovieImageExists($movie['poster_path'], "w200");
-
-                    echo "<img src='$imgPath'>";
-                    echo "<p class='title'>" . $movie['title'] . "</p>";
-                    echo "<span class='vote'>" . $movie['vote_average'] . "</span>";
-                    echo "</div>";
-                    echo "</a>";
-                }
-                ?>
-            </div>
-
-            <div class="page-filter">
-                <div class="filter-box">
-                    <?php
-                    // determines the base root of the url
-                    if (isset($data['movie_filter'])) {
-                        $base = $data['movie_filter'] . "/";
-                    } else {
-                        $base = "search/" . $data['query'] . "/";
-                    }
-
-                    // shows previous page
-                    if ($data['page'] > 1) {
-                        $prev = $data['page'] - 1;
-                        echo "<a href='/" . $base . $prev . "'> < </a>";
-                    }
-
-                    // shows page numbers
-                    for ($i = $data['minPage']; $i <= $data['maxPage']; $i++) {
-                        if ($i == $data['page']) {
-                            echo "<a class='current-page'> " . $i . "</a>";
-                        } else {
-                            echo "<a href='/" . $base . $i . "'> " . $i . "</a>";
-                        }
-                    }
-
-                    // shows next page
-                    if ($data['page'] < $data['total_pages']) {
-                        $next = $data['page'] + 1;
-                        echo "<a href='/" . $base . $next . "'> > </a>";
-                    }
-                    ?>
-                </div>
-            </div>
         </div>
-        <?php include_once __DIR__ . "/../included/footer.included.php" ?>
-    </body>
-</html>
+    </div>
+</div>
+
+<?= view_cell("\App\Libraries\ViewCells::footer") ?>
