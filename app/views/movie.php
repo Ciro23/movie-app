ce3f997bfa418694c392a99630cbea9d25488bfd
ciro23 <ciro.sorre23@gmail.com>
2022-02-17 11:47:40 +0100

    multiple changes: adapted to use ci4

--- app/views/movie.php
+++ app/views/movie.php
@@ -1,98 +1,94 @@
-<?php $imgPath = MovieModel::doesMovieImageExists($data['backdrop_path'], "original", "") ?>
-<!DOCTYPE html>
-<html lang="<?= $_COOKIE['language'] ?>" dir="ltr" style="background-image: url(<?= $imgPath ?>)">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?= $data['title'] ?> - Movie App</title>
+<?php $imgPath = App\Models\MovieModel::doesMovieImageExists($backdrop_path, "original", "") ?>
 
-        <!-- styles and other files -->
-        <?php include_once __DIR__ . "/../included/common.included.php" ?>
-        <link rel="stylesheet" href="/assets/styles/css/movie.style.css">
+<head>
+    <title><?= $title ?> - Movie App</title>
 
-        <!-- jQuery scripts -->
-        <script>
-            $(document).ready(function() {
-                var cast = <?= json_encode($data['cast']); ?>;
-                // shows all the cast
-                $(".show-all-cast-btn").on("click", function() {
-                    var env = <?= json_encode($_ENV); ?>;
+    <style>
+        html {
+            background-image: url(<?= $imgPath ?>);
+        }
+    </style>
 
-                    $(this).css("display", "none");
-                    $(".cast-grid").load("/assets/show-more/cast.show-more.php", {
-                        cast,
-                        env,
-                        includeCheck: true,
-                    });
+    <link rel="stylesheet" href="/assets/styles/css/movie.style.css">
+
+    <script>
+        $(document).ready(function() {
+            var cast = <?= json_encode($cast); ?>;
+            // shows all the cast
+            $(".show-all-cast-btn").on("click", function() {
+                var env = <?= json_encode($_ENV); ?>;
+
+                $(this).css("display", "none");
+                $(".cast-grid").load("/assets/show-more/cast.show-more.php", {
+                    cast,
+                    env,
+                    includeCheck: true,
                 });
-
-                if (cast.length <= 5) {
-                    $(".show-all-cast-btn").css("display", "none");
-                }
             });
-        </script>
-    </head>
 
-    <body>
-        <?php include_once __DIR__ . "/../included/nav-bar.included.php" ?>
+            if (cast.length <= 5) {
+                $(".show-all-cast-btn").css("display", "none");
+            }
+        });
+    </script>
+</head>
 
-        <div class="movie-container">
-            <div class="movie-details">
+<?= view_cell("\App\Libraries\ViewCells::header") ?>
 
-                <?php
-                $imgPath = MovieModel::doesMovieImageExists($data['poster_path'], "w300");
-                ?>
+<div class="movie-container">
+    <div class="movie-details">
 
-                <img src="<?= $imgPath ?>">
-                <div class="movie-overview">
-                    <?php
-                    echo "<h1>" . $data['title'] . "</h1>";
-                    echo "<span class='vote'>" . $data['vote_average'] . "</span>";
+        <?php
+        $imgPath = App\Models\MovieModel::doesMovieImageExists($poster_path, "w300");
+        ?>
 
-                    if ($data['isMovieInWatchlist']) {
-                        echo "<form method='POST' action='" . $_SERVER['REQUEST_URI'] . "/remove-from-watchlist' class='watchlist-button'>";
-                        echo "<button type='submit'>";
-                        echo "<img src='/public/assets/icons/bookmark_full.icon.png'>" . $lang['remove_from_watchlist'];
-                        echo "</button>";
-                        echo "</form>";
-                    } else {
-                        echo "<form method='POST' action='" . $_SERVER['REQUEST_URI'] . "/add-to-watchlist' class='watchlist-button'>";
-                        echo "<button type='submit'>";
-                        echo "<img src='/public/assets/icons/bookmark_empty.icon.png'>" . $lang['add_to_watchlist'];
-                        echo "</button>";
-                        echo "</form>";
-                    }
+        <img src="<?= $imgPath ?>">
+        <div class="movie-overview">
+            <?php
+            echo "<h1>" . $title . "</h1>";
+            echo "<span class='vote'>" . $vote_average . "</span>";
 
-                    echo "<p class='overview'>" . $data['overview'] . "</p>";
-                    echo "<p class='director'>" . $lang['director'] . ": " . $data['crew']['director'] . "</p>";
+            if ($isMovieInWatchlist) {
+                echo "<form method='POST' action='" . $_SERVER['REQUEST_URI'] . "/remove-from-watchlist' class='watchlist-button'>";
+                echo "<button type='submit'>";
+                echo "<img src='/public/assets/icons/bookmark_full.icon.png'>Remove from watchlist";
+                echo "</button>";
+                echo "</form>";
+            } else {
+                echo "<form method='POST' action='" . $_SERVER['REQUEST_URI'] . "/add-to-watchlist' class='watchlist-button'>";
+                echo "<button type='submit'>";
+                echo "<img src='/public/assets/icons/bookmark_empty.icon.png'>Add to watchlist";
+                echo "</button>";
+                echo "</form>";
+            }
 
-                    echo "<div class='genres'>";
-                    echo "<p>" . $lang['genres'] . ":</p>";
-                    for ($i = 0; $i < count($data['genres']); $i++) {
-                        echo "<p>" . $data['genres'][$i]['name'] . "</p>";
-                    }
-                    echo "</div>";
-                    ?>
-                </div>
-            </div>
-            <div class="movie-info">
-                <?php
-                echo "<p>" . $lang['release_date'] . ": " . $data['release_date'] . "</p>";
-                echo "<p>" . $lang['runtime'] . ": " . $data['runtime'] . "</p>";
-                echo "<p>" . $lang['budget'] . ": $" . $data['budget'] . "</p>";
-                ?>
-            </div>
-            <div class="cast">
-                <h2><?= $lang['actors'] ?></h2>
-                <span class="cast-grid">
-                    <?php
-                    $cast = $data['cast'];
-                    include_once __DIR__ . "/../included/cast.included.php";
-                    ?>
-                </span>
-                <button class="show-all-cast-btn"><?= $lang['show_all'] ?></button>
-            </div>
+            echo "<p class='overview'>" . $overview . "</p>";
+            echo "<p class='director'>Director: " . $crew['director'] . "</p>";
+
+            echo "<div class='genres'>";
+            echo "<p>Genres:</p>";
+            for ($i = 0; $i < count($genres); $i++) {
+                echo "<p>" . $genres[$i]['name'] . "</p>";
+            }
+            echo "</div>";
+            ?>
         </div>
-        <?php include_once __DIR__ . "/../included/footer.included.php" ?>
-    </body>
-</html>
+    </div>
+    <div class="movie-info">
+        <?php
+        echo "<p>Release date: " . $release_date . "</p>";
+        echo "<p>Runtime: " . $runtime . "</p>";
+        echo "<p>Budget: $" . $budget . "</p>";
+        ?>
+    </div>
+    <div class="cast">
+        <h2>Actors</h2>
+        <span class="cast-grid">
+            <?php
+            $cast = $cast;
+            include_once __DIR__ . "/../included/cast.included.php";
+            ?>
+        </span>
+        <button class="show-all-cast-btn">Show all</button>
+    </div>
+</div>
